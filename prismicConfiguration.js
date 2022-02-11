e69ad25b4412e271dc9843bf73b068c2da0987a1
Marco Kähler <m.kaehler@internetagentur-kaehler.de>
2022-02-11 10:07:42 +0100

    integration prismic with api key

--- prismicConfiguration.js
+++ prismicConfiguration.js
@@ -8,7 +8,7 @@ export const apiEndpoint = `https://${repoName}.cdn.prismic.io/api/v2`
 
 // -- Access Token if the repository is not public
 // Generate a token in your dashboard and configure it here if your repository is private
-export const accessToken = ''
+export const accessToken = process.env.PRISMIC_ACCESS_TOKEN
 
 // -- Link resolution rules
 // Manages the url links to internal Prismic documents
---
+++ slices/TextImageRight/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { RichText } from 'prismic-reactjs'
+
+const TextImageRight = ({ slice }: any) => (
+  <div className={slice.variation}>
+    <img src={slice.primary.image.url} alt={slice.primary.image.alt} />
+    <RichText render={slice.primary.title} />
+   </div>
+)
+
+export default TextImageRight
