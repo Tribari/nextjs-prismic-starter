f9161ac032e3373619c8b52df0d735bfd219eff6
Marco Kähler <m.kaehler@internetagentur-kaehler.de>
2022-02-15 11:12:37 +0100

    tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
 import { Client } from '../utils/prismicHelpers'
 import { RichText } from 'prismic-reactjs'
+import SliceZone from "next-slicezone"
+import * as Slices from "../slices"
+
+const resolver = ({ sliceName }) => Slices[sliceName]
 
 const Home = ({data}) => {
 
   if(data) {
     return (
       <div>
-        <RichText render={data.title} />
+        <RichText render={data.title}/>
         <RichText render={data.text} />
+        {data.slices.length > 0 && 
+          <SliceZone slices={data.slices} resolver={resolver} />
+        }
       </div>
     )
   }
--- slices/TextImageRight/index.js
+++ slices/TextImageRight/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { RichText } from 'prismic-reactjs'
 
-const TextImageRight = ({ slice }: any) => (
+const TextImageRight = ({ slice }) => (
   <div className={slice.variation}>
     <img src={slice.primary.image.url} alt={slice.primary.image.alt} />
     <RichText render={slice.primary.title} />
